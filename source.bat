e2785d1a4b7b84b3d32856a391e0165263c48bfb
AndreStorkYT <andrestork@outlook.it>
2022-07-09 21:08:33 +0200

    Added CUE convert support

--- source.bat
+++ source.bat
@@ -1,16 +1,16 @@
 @echo off
 :MAIN
 cls
-echo EASYCHD (v. Alpha22.03.17)
+echo EASYCHD (v. Alpha22.07.09)
 echo --------------------------------------
 echo 1) CUE or GDI to CHD
-echo 2) CHD to CUE (not working)
+echo 2) CHD to CUE
 echo 3) CHD to GDI (not working)
 echo 9) Exit
 echo --------------------------------------
 set /p mainaction=
 if %mainaction%==1 goto CHD
-if %mainaction%==2 goto MAIN
+if %mainaction%==2 goto CUE
 if %mainaction%==3 goto MAIN
 if %mainaction%==9 goto EXIT
 
@@ -31,6 +31,25 @@ cls
 echo DONE CONVERTING
 pause
 goto MAIN
+
+:CUE
+cls
+echo YOUR INPUT/OUTPUT FOLDER PATH: (you can drag and drop, spaces will not work if you're in PowerShell)
+set /p folderpath=
+cls
+xcopy %folderpath%
+for /r %%i in (*.chd) do chdman extractcd -i "%%i" -o "%%~ni.cue"
+xcopy *.bin %folderpath%
+xcopy *.cue %folderpath%
+del *.chd
+del *.cue
+del *.cdi
+del *.bin
+cls
+echo DONE CONVERTING
+pause
+goto MAIN
+
 
 :EXIT
 exit
